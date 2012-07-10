dd09fdc367ac4f73d72acba63031a311d501fd88
Anders Bakken <agbakken@gmail.com>
2012-07-10 14:50:55 -0700

    Strerror

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int i, err;
+    for (i=1; i<argc; ++i) {
+        err = atoi(argv[i]);
+        if (!err) {
+            printf("Can't parse %s\n", argv[i]);
+        } else {
+            printf("%d => %s\n", err, strerror(err));
+        }
+    }
+    return 0;
+}
